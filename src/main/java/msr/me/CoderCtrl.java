f59c702e3d141bb4d9a18c608b1636dac3297caa
Manny <egalli64@gmail.com>
2020-09-12 20:03:36 +0200

    first commit

---
+++ src/main/java/msr/me/CoderCtrl.java
@@ -0,0 +1,20 @@
+package com.example.demo.me;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CoderCtrl {
+    private CoderRepo repo;
+
+    public CoderCtrl(CoderRepo repo) {
+        this.repo = repo;
+    }
+
+    @GetMapping("/coders")
+    List<Coder> all() {
+        return repo.findAll();
+    }
+}
---
+++ src/main/java/msr/me/CoderRepo.java
@@ -0,0 +1,6 @@
+package com.example.demo.me;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CoderRepo extends JpaRepository<Coder, Long> {
+}
