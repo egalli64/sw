7093baac0e7407709857c61391dc9987137d2249
Manny <egalli64@gmail.com>
2020-09-13 16:49:36 +0200

    autowired repo

--- src/main/java/sbr/me/CoderCtrl.java
+++ src/main/java/sbr/me/CoderCtrl.java
@@ -4,17 +4,16 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CoderCtrl {
     private static final Logger LOG = LoggerFactory.getLogger(CoderCtrl.class);
-    private CoderRepo repo;
 
-    public CoderCtrl(CoderRepo repo) {
-        this.repo = repo;
-    }
+    @Autowired
+    private CoderRepo repo;
 
     @GetMapping("/coders")
     List<Coder> getAll() {
