e1091faeba81cf6065277627d84a6ae86a2ae42b
Manny <egalli64@gmail.com>
2020-09-13 16:06:48 +0200

    fix names

--- src/main/java/sbr/me/CoderCtrl.java
+++ src/main/java/sbr/me/CoderCtrl.java
@@ -2,11 +2,14 @@ package sbr.me;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CoderCtrl {
+    private static final Logger LOG = LoggerFactory.getLogger(CoderCtrl.class);
     private CoderRepo repo;
 
     public CoderCtrl(CoderRepo repo) {
@@ -14,7 +17,8 @@ public class CoderCtrl {
     }
 
     @GetMapping("/coders")
-    List<Coder> all() {
+    List<Coder> getAll() {
+        LOG.trace("enter getAll()");
         return repo.findAll();
     }
 }
