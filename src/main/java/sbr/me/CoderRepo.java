c33066042031b3a15e907d2a68dbf8b29dc8d7a8
Manny <egalli64@gmail.com>
2020-09-17 13:30:51 +0200

    annotated for clarity

--- src/main/java/sbr/me/CoderRepo.java
+++ src/main/java/sbr/me/CoderRepo.java
@@ -1,6 +1,8 @@
 package sbr.me;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CoderRepo extends JpaRepository<Coder, Long> {
 }
