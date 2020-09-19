df4055e2ccce6c71339c3b4458c6b4d9c2688321
Manny <egalli64@gmail.com>
2020-09-19 15:18:54 +0200

    cors enabled

--- src/main/java/sbr/me/CoderCtrl.java
+++ src/main/java/sbr/me/CoderCtrl.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class CoderCtrl {
     private static final Logger LOG = LoggerFactory.getLogger(CoderCtrl.class);
 
