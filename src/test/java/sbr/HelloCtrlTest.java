5db81a2c49bcb8aba47aa699b565c6a947f77056
Manny <egalli64@gmail.com>
2020-10-22 17:01:06 +0200

    mock test

---
+++ src/test/java/sbr/HelloCtrlTest.java
@@ -0,0 +1,26 @@
+package sbr;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class HelloCtrlTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testSayHello() throws Exception {
+        this.mockMvc.perform(get("/hello")) //
+                .andExpect(status().isOk()) //
+                .andExpect(jsonPath("$.message", is("Hello World!")));
+    }
+}
