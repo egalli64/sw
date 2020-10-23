f158a6d4a6b3b96b20f7775cc365e51e75fc1d0a
Manny <egalli64@gmail.com>
2020-10-23 08:28:29 +0200

    more test

--- src/test/java/sbr/HelloCtrlTest.java
+++ src/test/java/sbr/HelloCtrlTest.java
@@ -2,6 +2,7 @@ package sbr;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -9,18 +10,26 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 class HelloCtrlTest {
     @Autowired
-    private MockMvc mock;
+    private MockMvc mvc;
 
     @Test
     void getHello() throws Exception {
-        mock.perform(get("/hello")) //
+        mvc.perform(get("/hello")) //
                 .andExpect(status().isOk()) //
                 .andExpect(jsonPath("$.message", is("Hello World!")));
+    }
+
+    @Test
+    void putHello() throws Exception {
+        mvc.perform(put("/hello")) //
+                .andExpect(status().isMethodNotAllowed());
     }
 }
