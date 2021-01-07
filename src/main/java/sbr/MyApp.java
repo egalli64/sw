8e70acb4fe94f8e124fde09581db182f4097313d
Manny <egalli64@gmail.com>
2021-01-07 14:02:07 +0100

    bean example

--- src/main/java/sbr/MyApp.java
+++ src/main/java/sbr/MyApp.java
@@ -1,11 +1,22 @@
 package sbr;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MyApp {
-	public static void main(String[] args) {
-		SpringApplication.run(MyApp.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MyApp.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner beanCounter(ApplicationContext ctx) {
+        return args -> {
+            int count = ctx.getBeanDefinitionCount();
+            System.out.println(String.format("You have access to %d Spring Beans", count));
+        };
+    }
 }
