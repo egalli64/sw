0928bdccf4156b554176f0d13e9f9cae5a2927dd
Manny <egalli64@gmail.com>
2021-01-14 20:37:11 +0100

    moved to swr

--- src/main/java/sbr/me/CoderNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package sbr.me;
-
-public class CoderNotFoundException extends RuntimeException {
-    private static final long serialVersionUID = 1L;
-
-    CoderNotFoundException(Long id) {
-        super("Coder " + id + " not found");
-    }
-}
