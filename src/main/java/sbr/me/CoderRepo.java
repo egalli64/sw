91199ce458d51dd67cb6089189ca35f847240958
Manny <egalli64@gmail.com>
2021-01-14 11:22:20 +0100

    repo removed

--- src/main/java/sbr/me/CoderRepo.java
+++
@@ -1,8 +0,0 @@
-//package sbr.me;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface CoderRepo extends JpaRepository<Coder, Long> {
-//}
