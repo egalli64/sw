d01b74166b3b4d03d92f1b63a1d616c91da9d3ee
Emanuele <egalli64@gmail.com>
2022-10-18 11:55:39 +0200

    implicitly autowired

--- src/main/java/com/example/sw/s07/CoderCtr.java
+++ src/main/java/com/example/sw/s07/CoderCtr.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,8 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class CoderCtr {
     private static Logger log = LoggerFactory.getLogger(CoderCtr.class);
 
-    @Autowired
     private CoderSvc svc;
+
+    // implicit autowiring
+    public CoderCtr(CoderSvc svc) {
+        this.svc = svc;
+    }
 
     @GetMapping("/coders")
     public List<Coder> coders() {
