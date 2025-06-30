1a308908272421cfc98ff4468916a1ed0a733b78
Emanuele <egalli64@gmail.com>
2025-06-30 15:29:04 +0200

    context load test

---
+++ src/test/java/com/example/sw/SwApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.sw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Default test - check if the Spring application context loads successfully
+ */
+@SpringBootTest
+class SwApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
