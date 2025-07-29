6a47e03af82b4ee32e2ef78cc9399fd97c3c12e4
Emanuele <egalli64@gmail.com>
2025-07-29 15:41:07 +0200

    explicit logical folder by @RequestMapping

--- src/main/java/com/example/sw/s6/LoginController.java
+++ src/main/java/com/example/sw/s6/LoginController.java
@@ -5,13 +5,15 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/s6")
 public class LoginController {
     private static Logger log = LoggerFactory.getLogger(LoginController.class);
 
-    @PostMapping("/s6/login")
+    @PostMapping("/login")
     public String login(@RequestParam String user, @RequestParam String password, Model model) {
         log.trace("enter login({})", user);
 
